8438ab22d800ace51d8f5f8e37a6cb6b5afd4e11
PaoloDat <orsopaul007@gmail.com>
2018-08-26 19:51:32 +0300

    First commit. Let's start

---
+++ src/main/java/ru/vazisu/jackie/JackieApplication.java
@@ -0,0 +1,12 @@
+package ru.vazisu.jackie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JackieApplication.class, args);
+    }
+}
